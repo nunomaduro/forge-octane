a618e7985a131ca157be320836f7e362219df430
Nuno Maduro <enunomaduro@gmail.com>
2021-04-19 12:45:06 +0100

    wip

--- routes/console.php
+++ routes/console.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Laravel\Octane\Facades\Octane;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,10 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Artisan::command('concurrently', function () {
+    Octane::concurrently([
+        fn () => \App\Models\User::all(),
+        fn () => \App\Models\User::all(),
+    ]);
+});
